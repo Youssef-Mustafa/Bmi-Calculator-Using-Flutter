60d0624277e024aa7782206767a44bc08ec42fbd
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-07-22 22:15:10 +0300

    Add CustomAppBar and color theme

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Define Colors
+const kPrimaryColor = Color(0XFF0a0d22);
+const kSecondaryColor = Color(0XFF111428);
+const kButtonsColor = Color(0XFFeb1555);
+const kTextPrimaryColor = Color(0XFF868892);
+const kTextSecondaryColor = Colors.white;
---
+++ lib/views/home_view.dart
@@ -0,0 +1,71 @@
+import 'package:bmi_calculator/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: CustomAppBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24.0,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              FontAwesomeIcons.bars,
+              color: kTextSecondaryColor,
+            ),
+            Expanded(
+              child: Center(
+                child: Text(
+                  'BMI CALCULATOR',
+                  style: TextStyle(
+                    color: kTextSecondaryColor,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
